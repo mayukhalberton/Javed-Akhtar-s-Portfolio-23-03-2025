852eeef3afb9f2e2c9a422d635127e7193f698b6
Mayukh Bera <mayukhalberton@gmail.com>
2025-04-26 15:27:13 +0530

    work on mobile view

--- publications.js
+++ publications.js
@@ -11,10 +11,10 @@ document.getElementById('pub-3').addEventListener('click', (e) => {
     window.open('https://www.telegraphindia.com/culture/arts/learning-to-accept-all-that-is-dark-javed-akhtars-works-in-svikriti/cid/1897646', '_blank');
 });
 
-document.getElementById('pub-4').addEventListener('click', (e) => {
+document.getElementById('pub-5').addEventListener('click', (e) => {
     window.open('http://www.360doc24.net/wxarticlenew/1055050587.html?isnewgh=0&from=singlemessage&isappinstalled=0', '_blank');
 });
 
-// document.getElementById('pub-1').addEventListener('click', (e) => {
-//     window.open('https://www.traceyeminfoundation.com/tear26-exhibition', '_blank');
-// });
+document.getElementById('pub-4').addEventListener('click', (e) => {
+    window.open('https://drive.google.com/file/d/1iP7fkXg_2i4hRkgiDhwyDIWVO7Ims8Vr/view?usp=drive_link', '_blank');
+});
